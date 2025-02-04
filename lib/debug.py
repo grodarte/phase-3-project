ccabaf1fd0de442b246164c04fa5e75695e922fa
Gabrielle <ms.grodarte@gmail.com>
2025-02-03 16:31:42 -0800

    init start and end date

--- lib/debug.py
+++ lib/debug.py
@@ -2,6 +2,7 @@
 # lib/debug.py
 
 from models.__init__ import CONN, CURSOR
-
+from models.payperiod import PayPeriod
+from models.shift import shift
 
 breakpoint()
--- lib/models/PayPeriod.py
+++ lib/models/PayPeriod.py
@@ -1,3 +1,45 @@
+from datetime import date
+from models.__init__ import CURSOR, CONN
+
+
 class PayPeriod:
-    def __init__(self):
-        pass
+    
+    all = []
+
+    def __init__(self, start_month, start_day, start_year, end_month, end_day, end_year, id=None):
+        self.id = id
+        self._start_date = None
+        self._end_date = None
+        self.set_start_date(start_month, start_day, start_year)
+        self.set_end_date(end_month, end_day, end_year)
+
+    def __repr__(self):
+        return f'Pay Period {self.id}: {self.start_date} - {self.end_date}'
+    
+    @property
+    def start_date(self):
+        return self._start_date
+
+    def set_start_date(self, month, day, year):
+        try:
+            new_start_date = date(year, month, day)
+            if self._end_date and new_start_date > self._end_date:
+                raise ValueError("Start date cannot be after the end date.")
+            self._start_date = new_start_date
+        except ValueError as e:
+            print(f'Invalid start date: {e}')
+
+    @property
+    def end_date(self):
+        return self._end_date
+
+    def set_end_date(self, month, day, year):
+        try:
+            new_end_date = date(year, month, day)
+            if self._start_date and new_end_date < self._start_date:
+                raise ValueError("Start date cannot be after the end date.")
+            self._end_date = new_end_date
+        except ValueError as e:
+            print(f'Invalid end date: {e}')
+
+breakpoint()
