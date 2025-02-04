e0542d05dc6eebcde61c948b955f1ebb63cae348
Gabrielle <ms.grodarte@gmail.com>
2025-02-03 18:03:55 -0800

    initial cli options

--- lib/cli.py
+++ lib/cli.py
@@ -5,24 +5,18 @@ from helpers import (
     helper_1
 )
 
-
 def main():
-    while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            helper_1()
-        else:
-            print("Invalid choice")
 
+    payPeriods = []
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-
+    choice = 0
+    while choice != 4:
+        print("*** Pay Period Manager ***")
+        print("1. Add a pay period")
+        print("2. Lookup a pay period")
+        print("3. Display pay periods")
+        print("4. Quit")
+        choice = int(input())
 
 if __name__ == "__main__":
     main()
--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,11 @@
 
 from models.__init__ import CONN, CURSOR
 from models.payperiod import PayPeriod
-from models.shift import shift
+from models.shift import Shift
 
+def reset_database():
+    PayPeriod.drop_table()
+    PayPeriod.create_table()
+
+    
 breakpoint()
