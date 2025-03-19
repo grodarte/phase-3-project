0dc5d6116d4cf1535cc088205dffbe5f4206aea2
Gabrielle <ms.grodarte@gmail.com>
2025-03-18 18:40:50 -0700

    adds working menus

--- lib/cli.py
+++ lib/cli.py
@@ -2,42 +2,122 @@
 
 from helpers import (
     exit_program,
-    list_payperiods,
+    get_payperiods,
+    enumerate_payperiods,
+    create_payperiod,
+    update_payperiod,
+    calculate_payperiod_earnings,
+    get_shifts,
+    enumerate_shifts,
+    display_shift_details,
+    create_shift,
+    update_shift,
+    delete_shift
 )
 
 def main_menu():
-    '''Main menu for the earnings tracker'''
-
+    '''MAIN MENU FOR EARNINGS TRACKER'''
     choice = ''
-    exit_choice = 'e'
 
-    while choice != exit_choice:
-        print("***PERSONAL EARNINGS MANAGER***")
-        print("Hello...")
-        print('''Please choose from the following: ''')
-        print("Type P or p to view PAY PERIODS")
-        print("OR type E or e to exit")
+    while choice != 'e':
+        print("*** PERSONAL EARNINGS MANAGER ***")
+        print('''\nPlease choose from the following: ''')
+        print("\nType P or p to view the PAY PERIODS")
+        print("OR type E or e to EXIT")
+        choice = input(">>> ").strip().lower()
 
-        choice = input(">>> ")
         if choice == 'p':
-            list_payperiods()
-        else:
-            print("Invalid choice, please try again.")
+            show_payperiods_menu()
+        elif choice != 'e':
+            print("\nInvalid choice, please try again.")
 
     exit_program()
 
-def payperiods_menu():
+def show_payperiods_menu():
     '''Menu for interacting with payperiods'''
+    choice = ''
 
-    choice = 0
-    while choice != 4:
+    while choice != 'b':
+        payperiods = get_payperiods()
+        enumerate_payperiods(payperiods)
+        print("\nPlease select the number of the pay period to see shifts/calculate earnings")
+        print("             OR")
+        print("Type A or a to ADD a new Pay period")
+        print("Type B or b to go BACK to previous menu")
+        print("Type E or e to EXIT the program")
 
-        print()
+        choice = input(">>> ").strip().lower()
 
+        if choice.isdigit():
+            choice_num = int(choice)
+            if choice_num in range(1, len(payperiods)):
+                selected_payperiod = payperiods[choice_num - 1]
+                shifts_menu(selected_payperiod)
+            else:
+                print(f'\nInvalid pay period selection, please try again.')
+        elif choice == 'a':
+            create_payperiod()
+        elif choice == 'e':
+            exit_program()
+        elif choice != 'b':
+            print("\nInvalid choice, please try again.")
 
+def show_shifts_menu(selected_payperiod):
+    '''Menu for interacting with shifts'''
+    choice = ''
 
+    while choice != 'b':
+        shifts = get_shifts(selected_payperiod)
+        enumerate_shifts(shifts)
+        print("\nPlease select the number of the shift to see details")
+        print("               OR")
+        print("Type A or a to ADD a new shift to this pay period")
+        print("Type U or u to UPDATE this pay period's dates")
+        print("Type C or c to CALCULATE this pay period's total hours/earnings")
+        print("Type B or b to go BACK to previous menu")
+        print("Type E or e to EXIT the program")
 
+        choice = input(">>> ").strip().lower()
 
+        if choice.isdigit():
+            choice_num = int(choice)
+            if choice_num in range(1, len(shifts)):
+                selected_shift = shifts[choice_num - 1]
+                selected_shift_menu(selected_shift)
+            else:
+                print("\nInvalid shift selection, please try again.")
+        elif choice == 'a':
+            create_shift(selected_payperiod.id)
+        elif choice == 'u':
+            update_payperiod(selected_payperiod)
+        elif choice == 'c':
+            calculate_payperiod_earnings(selected_payperiod)
+        elif choice == 'e':
+            exit_program()
+        elif choice != 'b':
+            print("\nInvalid choice, please try again.")
+
+def selected_shift_menu(selected_shift):
+    choice = ''
+
+    while choice != 'b':
+        display_shift_details(selected_shift)
+        print("\nPlease choose from the following:")
+        print("\nType U or u to UPDATE this shift")
+        print("Type D or d to DELETE this shift")
+        print("Type B or b to go BACK to the previous menu")
+        print("Type E or e to EXIT the program")
+
+        choice = input(">>> ").strip().lower()
+
+        if choice == 'u':
+            update_shift(selected_shift)
+        elif choice == 'd':
+            delete_shift(selected_shift)
+        elif choice == 'e':
+            exit_program()
+        elif choice != 'b':
+            print("\nInvalid choice, please try again.")
 
 if __name__ == "__main__":
     main_menu()
--- lib/helpers.py
+++ lib/helpers.py
@@ -2,58 +2,72 @@
 from models.shift import Shift
 from models.payperiod import PayPeriod
 
-def list_payperiods():
+def exit_program():
+    print("Exiting program. Goodbye!")
+    exit()
+
+def get_payperiods():
+    pass
+
+def enumerate_payperiods(payperiods):
     pass
 
 def create_payperiod():
     pass
 
-def view_shifts_in_payperiod():
+def update_payperiod(payperiod_obj):
+    pass
+
+def calculate_payperiod_earnings(payperiod_obj):
+    pass
+
+def get_shifts(payperiod_obj):
+    pass
+
+def enumerate_shifts(shifts):
+    pass
+
+def display_shift_details(shift_obj):
+    pass
+
+def create_shift(payperiod_id):
+    pass
+
+def update_shift(shift_obj):
+    pass
+
+def delete_shift(shift_obj):
     pass
 
 
+# def hours_worked(time_in, time_out):
+#     """Calculate hours worked as a decimal number."""
+#     h_in, m_in = map(int, self.clock_in.split(":"))
+#     h_out, m_out = map(int, self.clock_out.split(":"))
 
-def hours_worked(time_in, time_out):
-    """Calculate hours worked as a decimal number."""
-    h_in, m_in = map(int, self.clock_in.split(":"))
-    h_out, m_out = map(int, self.clock_out.split(":"))
+#     total_minutes = (h_out * 60 + m_out) - (h_in * 60 + m_in)
+#     if total_minutes < 0:
+#         raise ValueError("Clock-out time must be after clock-in time")
 
-    total_minutes = (h_out * 60 + m_out) - (h_in * 60 + m_in)
-    if total_minutes < 0:
-        raise ValueError("Clock-out time must be after clock-in time")
+#     return round(total_minutes / 60, 2)
 
-    return round(total_minutes / 60, 2)
+# def total_tips(cc, cash):
+#     return cc + cash
 
-def total_tips(cc, cash):
-    return cc + cash
-
-#NEED TO UPDATE, piece by piece validation
-def create_shift():
-    print("Creating shift fn")
-    year = input("Enter the shift year as 'YY': ")
-    month = input("Enter the shift month: ")
-    day = input("Enter the shift day: ")
-    clock_in = input("Enter clock-in time as 'HH:MM AM/PM': ")
-    clock_out = input("Enter clock-out time as 'HH:MM AM/PM': ")
-    cc_tip = input("Enter tips earned via credit card: ")
-    cash_tip = input("Enter cash tips earned: ")
-    try:
-        shift = Shift.create(year, month, day, clock_in, clock_out, cc_tip, cash_tip)
-        print(f"Shift on {self.month}/{self.day}/{self.year} | In: {self.clock_in} | Out: {self.clock_out} | "
-                f"Hours: {self.hours_worked(clock_in, clock_out)} | Tips: ${self.total_tips(cc_tip, cash_tip):.2f}")
-        # replace with repr f string
-    except Exception as exc:
-        print("Error creating shift: ", exc)
-
-def view_shift():
-    pass
-
-def edit_shift():
-    pass
-
-def delete_shift():
-    pass
-
-def exit_program():
-    print("Exiting program. Goodbye!")
-    exit()
+# #NEED TO UPDATE, piece by piece validation
+# def create_shift():
+#     print("Creating shift fn")
+#     year = input("Enter the shift year as 'YY': ")
+#     month = input("Enter the shift month: ")
+#     day = input("Enter the shift day: ")
+#     clock_in = input("Enter clock-in time as 'HH:MM AM/PM': ")
+#     clock_out = input("Enter clock-out time as 'HH:MM AM/PM': ")
+#     cc_tip = input("Enter tips earned via credit card: ")
+#     cash_tip = input("Enter cash tips earned: ")
+#     try:
+#         shift = Shift.create(year, month, day, clock_in, clock_out, cc_tip, cash_tip)
+#         print(f"Shift on {self.month}/{self.day}/{self.year} | In: {self.clock_in} | Out: {self.clock_out} | "
+#                 f"Hours: {self.hours_worked(clock_in, clock_out)} | Tips: ${self.total_tips(cc_tip, cash_tip):.2f}")
+#         # replace with repr f string
+#     except Exception as exc:
+#         print("Error creating shift: ", exc)
